adcc701a6c42c8c2332728bbc4b4aceabdf4d591
Kevin912215 <duankaiwen89@gmail.com>
2024-10-04 20:21:09 +0200

    Add files via upload

---
+++ plus excercise/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ss = 6;
+    float dd = '3,14';
+    char aa = 'W';
+    printf("I'm %d and I have %f %c", ss, dd, aa );
+    return 0;
+}
