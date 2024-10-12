ed87c16efd977de6c2771effb6dc9c68c30899fb
Kevin912215 <duankaiwen89@gmail.com>
2024-10-12 21:27:15 +0200

    Add files via upload

---
+++ selection/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int w;
+    printf("Please enter a number:");
+    scanf("%d",&w);
+    if (w < 10)
+       {
+        printf("small");
+        printf("small");
+       }
+    else
+        printf("big");
+    return 0;
+}
