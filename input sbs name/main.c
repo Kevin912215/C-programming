a2c5ddf9d76a14c2a85f917fc9d768b8a694d1de
Kevin912215 <duankaiwen89@gmail.com>
2024-10-17 22:45:59 +0200

    Add files via upload

---
+++ input sbs name/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char K;
+    char W;
+    printf("Please input a character:\n");
+    scanf("%c%c",&K,&W);
+    printf("My name is %c,%c\n", K,W);
+    /*printf("Please input a character!\n");
+    scanf("%c",&W);
+    printf("My name is %c", W);*/
+    return 0;
+}
